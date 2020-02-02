aedfd09830177587e4ee72b6289a198a22ce9eb7
Michael Matus <michael@matus.at>
2020-02-02 20:33:36 +0100

    Initial check-in of module NmmStageMicro

---
+++ NmmStageMicro/Options.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace NmmStageMicro
+{
+    class Options
+    {
+        [Option('X', "X-axis", DefaultValue = "XYvec", HelpText = "Channel used as x-axis")]
+        public string Xaxis { get; set; }
+
+        [Option('Z', "Z-axis", DefaultValue = "AX", HelpText = "Channel used as z-axis")]
+        public string Zaxis { get; set; }
+
+        [Option("precision", DefaultValue = 2, HelpText = "Decimal digits for results")]
+        public int iPrecision { get; set; }
+
+        [Option('d', "div", DefaultValue = 0, HelpText = "Nominal scale division in um")]
+        public double fDiv { get; set; }
+
+        [Option('n', "nlines", DefaultValue = 0, HelpText = "Expected number of line marks.")]
+        public int expcTargets { get; set; }
+
+        [Option('r', "reference", DefaultValue = 0, HelpText = "Line # to reference to.")]
+        public int iRef { get; set; }
+
+        [Option("alpha", DefaultValue = 0.0, HelpText = "Thermal expansion coefficient in 1/K")]
+        public double alpha { get; set; }
+
+        [Option('m', "morpho", DefaultValue = 25, HelpText = "Kernel value of morphological filter.")]
+        public int iMorpho { get; set; }
+
+        [Option('t', "threshold", DefaultValue = 0.5, HelpText = "Threshold value for line detection.")]
+        public double fThre { get; set; }
+
+        [Option('q', "quiet", HelpText = "Quiet mode. No screen output (except for errors).")]
+        public bool Quiet { get; set; }
+
+        [Option("heydemann", HelpText = "Perform Heydemann correction.")]
+        public bool Heyde { get; set; }
+
+        [Option("back", HelpText = "Use backtrace profile (when present).")]
+        public bool bBack { get; set; }
+
+        [Option("both", HelpText = "Mean of forward and backtrace profile (when present).")]
+        public bool bBoth { get; set; }
+
+
+        [ValueList(typeof(List<string>), MaximumElements = 2)]
+        public IList<string> ListOfFileNames { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            string AppName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            string AppVer = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            HelpText help = new HelpText
+            {
+                Heading = new HeadingInfo(AppName, "version " + AppVer),
+                Copyright = new CopyrightInfo("Michael Matus", 2015),
+                AdditionalNewLineAfterOption = false,
+                AddDashesToOption = true
+            };
+            string sPre = "Program to evaluate scanning files by SIOS NMM-1 for calibrating stage micrometers using the laser focus probe. " +
+                "For multiple profiles the line marks are detected separatly and average position are calculated. " +
+                "The number of line marks must be provided (via -n option). The nominal scale divison (-d) is needed for evaluation of deviations. " +
+                " ";
+            help.AddPreOptionsLine(sPre);
+            help.AddPreOptionsLine("");
+            help.AddPreOptionsLine("Usage: " + AppName + " filename1 [filename2] [options]");
+            // help.AddPostOptionsLine("Supported values for --filetype: 1=*.txt, 2=*.sig, 3=*.prf, 4=*.pr, 5=*.sdf, 6=*.smd");
+            help.AddOptions(this);
+            return help;
+        }
+    }
+}
---
+++ NmmStageMicro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Bev.IO.NmmReader;
+using Bev.IO.NmmReader.scan_mode;
+
+namespace NmmStageMicro
+{
+    class MainClass
+    {
+        static readonly Options options = new Options();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
